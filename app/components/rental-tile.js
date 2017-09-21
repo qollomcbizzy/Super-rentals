cb5c7af7df9bf854983ee0b77c634fe9ae5b3635
collo <qollomcbizzy64@gmail.com>
2017-09-21 10:39:13 +0300

    added the delete function in the rental-tile.hbs and rental-tile.js

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  IsImageShowing:false,
+  actions :{
+    imageShow : function(){
+      this.set('IsImageShowing',true);
+    },
+    imageHide:function(){
+      this.set('IsImageShowing',false);
+    },
+    delete(rental){
+      if(confirm('Are you sure you want to delete this')){
+        this.sendAction('destroyRental',rental)
+      }
+    }
+  }
 });
