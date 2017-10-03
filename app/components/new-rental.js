b5128a4d28717cf75a29215db9f34ac9fa9e09d0
collo <qollomcbizzy64@gmail.com>
2017-10-03 14:09:13 +0300

    added the saveRental action

--- app/components/new-rental.js
+++ app/components/new-rental.js
@@ -1,10 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  addNewRental:false;
+  addNewRental:false,
   actions:{
     rentalShowForm(){
-      this.set('addNewRental',true)
+      this.set('addNewRental',true);
+    },
+    saveRental1() {
+      var params = {
+        owner: this.get('owner'),
+        city: this.get('city'),
+        type: this.get('type'),
+        image: this.get('image'),
+        bedrooms: this.get('bedrooms'),
+      };
+      this.set('addNewRental', false);
+      this.sendAction('saveRental2', params);
     }
   }
 });
