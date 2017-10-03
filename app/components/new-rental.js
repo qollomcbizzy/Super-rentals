5ab678a7c99b08658121854fa0b98eb2ad535c9f
collo <qollomcbizzy64@gmail.com>
2017-10-03 13:55:48 +0300

    added the new-rental.hbs,generated the new-rental component and set the addNewRental to false  or true after the action in the new-rental.js

--- app/components/new-rental.js
+++ app/components/new-rental.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewRental:false;
+  actions:{
+    rentalShowForm(){
+      this.set('addNewRental',true)
+    }
+  }
 });
