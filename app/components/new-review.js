0bf3014389a7ff8bcae7d71b5dd1c825a2ef3706
collo <qollomcbizzy64@gmail.com>
2017-10-08 18:02:52 +0300

    added the new-review.js action to send to firebase

--- app/components/new-review.js
+++ app/components/new-review.js
@@ -4,6 +4,17 @@ export default Ember.Component.extend({
   addReview:false;
   actions:
   {
-    
+    reviewForm(){
+      this.set('addReview',true);
+    },
+    saveReview(){
+      var params:{
+        author:this.get('author'),
+        rating:this.get('rating'),
+        content:this.get('content')
+      };
+      this.set('addReview',false);
+      this.sendAction('saveReview',params);
+    }
   }
 });
