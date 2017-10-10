f725d847e9f99a6d61b3f485d28d4a668118becc
collo <qollomcbizzy64@gmail.com>
2017-10-10 21:44:48 +0300

    added the rental-popularity.js and to the index.hbs

--- app/helpers/rental-popularity.js
+++ app/helpers/rental-popularity.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export function rentalPopularity(params/*, hash*/) {
-  return params;
+  var rental = params[0];
+
+ if(rental.get('reviews').get('length') >= 5) {
+   return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
+   //ember.string.htmlSafe it allows us to render the html line
 }
 
 export default Ember.Helper.helper(rentalPopularity);
