dcc27da07fe0e0942a27fc2bd8435357e37a092c
collo <qollomcbizzy64@gmail.com>
2017-10-10 08:36:34 +0300

    added the delete review both from the parent rental-datil to the child review-tile

--- app/components/review-tile.js
+++ app/components/review-tile.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions:
+  {
+    delete(review){
+      if(confirm("Are you  sure you want to delete the review ?")){
+        this.sendAction('destroyReview',review);
+      }
+    }
+  }
 });
