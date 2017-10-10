f631c4f9159f69e1c2f2ffd04b9bca4ac02b891f
collo <qollomcbizzy64@gmail.com>
2017-10-10 22:20:36 +0300

    added the content to the rental-cost for the check of the cost

--- app/helpers/rental-cost.js
+++ app/helpers/rental-cost.js
@@ -2,6 +2,22 @@ import Ember from 'ember';
 
 export function rentalCost(params/*, hash*/) {
   return params;
+   var rentalPrice = params[0].get('cost');
+   if(rentalPrice >= 100){
+     return '$$$$';
+   }
+   else if(rentalPrice >= 100){
+     return '$$$';
+   }
+   else if(rentalPrice >=50){
+     return '$$';
+   }
+   else if(rentalPrice =< 49){
+     return '$';
+   }
+   else{
+     alert ('enter an appropriate rental price');
+   }
 }
 
 export default Ember.Helper.helper(rentalCost);
